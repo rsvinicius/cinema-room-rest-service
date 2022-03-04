1762e474ab6d0a14875a63da03c26852c0c09fc9
Vinicius R <vrodrigues.code@gmail.com>
2022-03-04 10:01:14 -0300

    initial commit

---
+++ src/main/java/com/example/cinemaroomrestservice/CinemaRoomRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemaroomrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaRoomRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaRoomRestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cinemaroomrestservice/CinemaRoomRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemaroomrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaRoomRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
